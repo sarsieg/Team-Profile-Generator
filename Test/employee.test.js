42f25c91caccffd85402882c2bcf344e90737d88
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-22 20:53:46 -0500

    adding more tests

--- Test/employee.test.js
+++ Test/employee.test.js
@@ -11,3 +11,38 @@ test("Can get Id via getId()", () => {
     const e = new Employee("Bob", testValue);
     expect(e.getId()).toBe(testValue);
 });
+
+test("Can get name via getName()", () => {
+    const testValue = "Frank";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+});
+
+test("Can instantiate Employee instance", () => {
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
+});
+
+test("Can set email via constructor argument", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("Bob", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test("getRole() should return \"Employee\"", () => {
+    const testValue = "Employee";
+    const e = new Employee("Frank", 1, "test@test.com", "Employee");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get email via getEmail()", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("Bob", 1, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
+
+test("Can get id via getId()", () => {
+    const testValue = 100;
+    const e = new Employee("Bob", testValue);
+    expect(e.getId()).toBe(testValue);
+});
