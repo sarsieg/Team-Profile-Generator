feb480ddd79908173e7310652a17d67078eaefc1
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-22 21:22:11 -0500

    added tests for manager files

---
+++ Test/manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require("../Lib/manager");
+const Employee = require("../Lib/employee");
+
+test("Can set office number via constructor", () => {
+    const testValue = 100;
+    const e = new Manager("Bob", 1, "test@test.com", testValue, "Manager");
+    expect(e.officeNumber).toBe(testValue);
+});
+
+test("Can get office number via getOffice()", () => {
+    const testValue = 100;
+    const e = new Manager("Bob", 1, "test@test.com", testValue, "Manager");
+    expect(e.getOfficeNumber).toBe(testValue);
+});
+
+test("getRole() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const e = new Manager("Bob", 1, "test@test.com", 100, "Manager");
+    expect(e.getRole()).toBe(testValue);
+});
