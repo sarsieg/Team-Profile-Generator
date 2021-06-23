b0dc6d3e047098cce560d31b0535d5fdfcbf7856
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-22 20:26:06 -0500

    added class and constructor for engineer to get and return the information and export it to the module

---
+++ Lib/engineer.js
@@ -0,0 +1,18 @@
+const Employee = require("./employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github, title) {
+        super(name, id, email);
+        this.github = github;
+        this.title = title;
+    }
+    getGithub() {
+        return this.github;
+    }
+
+    getRole() {
+        return this.title;
+    }
+}
+
+module.exports = Engineer;
