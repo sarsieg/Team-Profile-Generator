0a367a110ca74cb276757e8791dc9cb1909b8a70
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-22 20:28:49 -0500

    added constructor and function to return the information given within intern

---
+++ Lib/intern.js
@@ -0,0 +1,17 @@
+const Employee = require("./employee");
+
+class Intern extends Employee {
+    constructor(name, id, email, school, title) {
+        super(name, id, email);
+        this.school = school;
+        this.title = title;
+    }
+    getRole() {
+        return this.title;
+    }
+    getSchool() {
+        return this.school;
+    }
+}
+
+module.exports = Intern;
