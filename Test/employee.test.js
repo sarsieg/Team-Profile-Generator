14b0244b5a908099b6275255aa33fb591574cc68
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-22 21:03:27 -0500

    finished tests for employee

--- Test/employee.test.js
+++ Test/employee.test.js
@@ -46,3 +46,21 @@ test("Can get id via getId()", () => {
     const e = new Employee("Bob", testValue);
     expect(e.getId()).toBe(testValue);
 });
+
+test("Can get name via getName()", () => {
+    const testValue = "Frank";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+});
+
+test("Can set id via constructor argument", () => {
+    const testValue = 100;
+    const e = new Employee("Bob", testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test("Can set name via constructor argument", () => {
+    const name = "Frank";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
